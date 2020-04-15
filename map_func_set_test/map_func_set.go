0e535dce1fdbf1543f3c64c2ccc5fdd0e2d81a91
Henry Zhang <sunnywalden@gmail.com>
2020-04-15 16:27:10 +0800

    finish map set

--- map_func_set_test/map_func_set.go
+++ map_func_set_test/map_func_set.go
@@ -11,3 +11,17 @@ func TestMap(t *testing.T) {
 		t.Log(m1["itself"](v))
 	}
 }
+
+func TestSet(t *testing.T) {
+	s1 := map[string]bool{}
+	s1["SunnyWalden"] = true
+	s1["Henry"] = true
+	user1 := "Henry"
+	user2 := "Walden"
+	if s1[user1] {
+		t.Log("User %s already exists", user1)
+	} else {
+		s1[user2] = true
+		t.Log("User %s registry success!", user2)
+	}
+}
