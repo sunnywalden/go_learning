39aaa0a8f54af08b68dc2ede17c4696b7ecddda8
Henry Zhang <sunnywalden@gmail.com>
2020-04-16 19:27:04 +0800

    fix interface using bug

--- interface_practise/interface_practise_test.go
+++ interface_practise/interface_practise_test.go
@@ -8,14 +8,17 @@ import (
 type SqlConn string
 
 type LogOut interface {
-		LogOutput(err error,res ...interface{})
+	LogOutput(err error,res ...interface{})
 }
 
-func LogOutput(err error,res ...interface{}) {
+type Logger struct {
+}
+
+func (l *Logger) LogOutput(err error,res ...interface{}) {
 	if err == nil {
 		fmt.Printf("Result: %s", res)
 	} else {
-		fmt.Errorf("Error occured %s", err)
+		fmt.Printf("Error occurred %s", err)
 	}
 }
 
@@ -28,46 +31,57 @@ type CloseConn interface {
 }
 
 
+
 type SqlCon interface {
 	GetConn
 	CloseConn
 }
 
-type SqlQuery interface {
-	SqlQuering(con SqlConn, sql string) (string, error)
+
+type GetSqlConn struct {
 }
 
-type SqlUpdate interface {
-	SqlUpdating(con SqlConn, sql string) (bool, error)
-}
-
-func GetConnection() SqlConn {
+func (g *GetSqlConn) GetConnection() SqlConn {
 	fmt.Print("Connecting now!\n")
 	fmt.Print("Connection info!\n")
 	return "Fake Sql connection"
 }
 
-func CloseConnection(con SqlConn) (res bool, err error) {
+type CloseSqlConn struct {
+}
+
+func (c *CloseSqlConn) CloseConnection(con SqlConn) (res bool, err error) {
 	fmt.Printf("Closing connection %s now!\n", con)
 	return true,nil
 }
 
-func SqlQuering(con SqlConn, sql string) (res string, err error) {
+type SqlQueryor struct {
+}
+
+func (q *SqlQueryor) SqlQuering(con SqlConn, sql string) (res string, err error) {
 	fmt.Printf("Execute sql %s now!\n", sql)
 	return "fake result",nil
 }
 
-func SqlUpdating(con SqlConn, sql string) (bool, error) {
+type SqlUpdator struct {
+}
+
+func (u *SqlUpdator) SqlUpdating(con SqlConn, sql string) (bool, error) {
 	fmt.Printf("Update sql %s now!\n", sql)
 	return true, nil
 }
 
 func TestPolymorphism(t *testing.T) {
-	conn := GetConnection()
-	query_res,query_err := SqlQuering(conn, "fake sql")
-	LogOutput(query_err, query_res)
-	update_res,update_err := SqlUpdating(conn, "fake updating sql")
-	LogOutput(update_err, update_res)
-	close_res, close_err := CloseConnection(conn)
-	LogOutput(close_err, close_res)
+	con := &GetSqlConn{}
+	conn := con.GetConnection()
+	l := &Logger{}
+	q := &SqlQueryor{}
+	query_res,query_err := q.SqlQuering(conn, "fake sql")
+	l.LogOutput(query_err, query_res)
+	u := &SqlUpdator{}
+	update_res,update_err := u.SqlUpdating(conn, "fake updating sql")
+	l.LogOutput(update_err, update_res)
+	c := &CloseSqlConn{}
+	close_res, close_err := c.CloseConnection(conn)
+	l.LogOutput(close_err, close_res)
 }
