b42c023d0d63ee00d093c8280367c8ed2383d7ab
zhangbo <zhangbo@tezign.com>
2020-04-23 11:49:54 +0800

    first response

--- multichannel_timeout/first_response.go
+++ multichannel_timeout/first_response.go
@@ -1,14 +1,29 @@
 package multichannel_timeout
 
+//func FistRadio(MusicChan, NewsChan chan string) string {
+//	MusicChannel(MusicChan)
+//	NewsChannel(NewsChan)
+//	select {
+//		case music := <- MusicChan:
+//			return music
+//			case news := <- NewsChan:
+//				return news
+//	default:
+//		return "No channel"
+//	}
+//}
+
+
 func FistRadio(MusicChan, NewsChan chan string) string {
+	FirstChan := make(chan string)
 	MusicChannel(MusicChan)
 	NewsChannel(NewsChan)
-	select {
-		case music := <- MusicChan:
-			return music
-			case news := <- NewsChan:
-				return news
-	default:
-		return "No channel"
+	AllChan := []chan string{MusicChan, NewsChan}
+	for _,CurrentChan := range AllChan {
+		go func(CurrentChan chan string,FirstChan chan string) {
+			res := <- CurrentChan
+			FirstChan <- res
+		}(CurrentChan,FirstChan,)
 	}
+	return <-FirstChan
 }
