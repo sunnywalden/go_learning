b9da7d72c0c29cb90c8bf05337a790e44962adbe
zhangbo <zhangbo@tezign.com>
2020-04-27 10:36:17 +0800

    finish bdd

---
+++ bdd_demo/bdd_practise_test.go
@@ -0,0 +1,14 @@
+package bdd_demo
+
+import (
+	"testing"
+	"time"
+)
+import "github.com/smartystreets/goconvey/convey"
+
+func TestConvey(t *testing.T) {
+	Convey("Given birth",t , func() {
+		BirthDay := time.Da
+		RightNow ：= time.Now()
+	})
+}
