31e7d21b3ec6047d534571b65cf52e13b0207b7b
Henry Zhang <sunnywalden@gmail.com>
2020-05-27 23:21:57 +0800

    effective map\

--- effective_lock/concurent_map.go
+++ effective_lock/concurent_map.go
@@ -1,29 +1,23 @@
 package effective_lock
 
 import (
+	"errors"
+
 	"github.com/orcaman/concurrent-map"
 )
 
-func ConcurrentMapGet(m cmap.ConcurrentMap, name string) (info []string, err error) {
-	res, err := m.get(name)
-	if err != nil {
-		return nil, err
+func ConcurrentMapGet(m cmap.ConcurrentMap, name string) (info interface{}, err error) {
+	res, ok := m.Get(name)
+	if !ok {
+		return nil, errors.New("get failed")
 	}
 	return res, nil
 }
 
-func ConcurrentMapSet(m cmap.ConcurrentMap, name string, info []string) (success bool, err error) {
-	_, setErr := m.set(name, info)
-	if setErr != nil {
-		return false, setErr
-	}
-	return true, nil
+func ConcurrentMapSet(m cmap.ConcurrentMap, name string, info []string) () {
+	m.Set(name, info)
 }
 
-func ConcurrentMapDel(m cmap.ConcurrentMap, name string) (success bool, err error) {
-	_, delErr := m.del(name)
-	if delErr != nil {
-		return false, delErr
-	}
-	return true, nil
+func ConcurrentMapDel(m cmap.ConcurrentMap, name string) () {
+	m.Remove(name)
 }
--- effective_lock/map_test.go
+++ effective_lock/map_test.go
@@ -4,13 +4,44 @@ import (
 	"testing"
 
 	"github.com/orcaman/concurrent-map"
+	"github.com/sunnywalden/go_learning/effective_lock"
 )
 
-func BenchmarkSyncMap(b *testing.B) {
-	b.StartTimer()
-	m := cmap.New()
+
+var waldenInfo = []string{"SunnyWalden", "30"}
+var henryInfo  = []string{"Henry Zhang", "28"}
+
+
+func initData(userInfo []string) (res []string) {
 	var info []string
-	info = append(info, "SunnyWalden", "30")
-	ConcurrentMapSet(m, "walden", info)
+	for _,use := range userInfo {
+		info = append(info, use)
+	}
+	return info
+}
+
+
+func BenchmarkSyncMapSet(b *testing.B) {
+	m := cmap.New()
+
+
+	b.StartTimer()
+	effective_lock.ConcurrentMapSet(m, "walden", waldenInfo)
+	//effective_lock.ConcurrentMapSet(m, "henry", henryInfo)
+	b.StopTimer()
+}
+
+func BenchmarkSyncMapGet(b *testing.B) {
+	m := cmap.New()
+	effective_lock.ConcurrentMapSet(m, "walden", waldenInfo)
+	effective_lock.ConcurrentMapSet(m, "henry", henryInfo)
+
+	b.StartTimer()
+	info ,err := effective_lock.ConcurrentMapGet(m, "walden")
+	if err != nil {
+		b.Log(err)
+	} else {
+		b.Logf("%s info %s", "walden", info)
+	}
 	b.StopTimer()
 }
